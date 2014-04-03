b10ae7af133cdbad031c5f1ff8c9a01d7e20d6be
leffen <leffen@gmail.com>
2014-04-03 15:35:58 +0200

    Versjon 3

--- public/ad3.js
+++ public/ad3.js
@@ -8,17 +8,11 @@ var app = angular.module('ad3', ["nvd3ChartDirectives", "ngResource", "ad3_utils
     function ($scope, Data, Utils) {
       $scope.info = {};
       $scope.info.title = 'Transkodingstid / lengde';
-      $scope.info.data = [
-        {key: "border_line", values: []}
-      ];
+      $scope.info.data = [{key: "border_line", values: []}];
 
       Data.$promise.then(function (data) {
-        $scope.info.data = [
-          {key: 'assets', values: data},
-          {key: "border_line", values: [
-            [0, 1200],
-            [450, 2550]
-          ]}
+        $scope.info.data = [{key: 'assets', values: data},
+                            {key: "border_line", values: [[0, 1200],[450, 2550]]}
         ];
       });
 
@@ -30,5 +24,37 @@ var app = angular.module('ad3', ["nvd3ChartDirectives", "ngResource", "ad3_utils
           return 'red';
         };
       };
+      $scope.infoTooltip = function(){
+        return function(key, x, y, e, graph) {
+          if(e.seriesIndex==1) return "";
+          return  'Videoklipp' +
+            '<h1>' + e.point[2] + '</h1>' +
+            '<p> server:' + e.point[4]+ ' - format:' + e.point[3] + '</p>';
+        };
+      };
+
+      $scope.callback = function(){
+        return function(chart){
+
+          chart.lines.dispatch.on('elementClick', function(e){
+            $scope.show_asset(e);
+          });
+
+        };
+      };
+
+      $scope.show_asset = function(e) {
+        console.log(e);
+        $scope.info.asset_id = e.point[2];
+        $scope.info.server = e.point[4];
+        $scope.info.format = e.point[3];
+        $scope.info.lengde = e.point[0];
+
+
+
+        $scope.$digest();
+      };
+
+
     }]);
 
